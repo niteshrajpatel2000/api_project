a62e283f6b46443da6187506dc547773b14189a8
NITESHRAJPATEL\nites <niteshrajpatel.edugaon@gmail.com>
2025-10-31 13:29:46 +0530

    added welcome api

--- main.py
+++ main.py
@@ -5,3 +5,6 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Hello, FastAPI is working!"}
+@app.get("/welcome")
+def hello():
+    return {"message": "👋 Welcome to your FastAPI server on Render! 🚀"}
